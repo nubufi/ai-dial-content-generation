a5249750348de4984944f86652570942031583ba
numanburakfidan <numanburakfidan@yandex.com>
2026-01-08 14:47:04 +0300

    update

--- task/image_to_text/task_dial_itt.py
+++ task/image_to_text/task_dial_itt.py
@@ -11,34 +11,48 @@ from task._models.role import Role
 
 
 async def _put_image() -> Attachment:
-    file_name = 'dialx-banner.png'
+    file_name = "dialx-banner.png"
     image_path = Path(__file__).parent.parent.parent / file_name
-    mime_type_png = 'image/png'
-    # TODO:
-    #  1. Create DialBucketClient
-    #  2. Open image file
-    #  3. Use BytesIO to load bytes of image
-    #  4. Upload file with client
-    #  5. Return Attachment object with title (file name), url and type (mime type)
-    raise NotImplementedError
+    mime_type_png = "image/png"
+
+    async with DialBucketClient(api_key=API_KEY, base_url=DIAL_URL) as bucket_client:
+        with open(image_path, "rb") as image_file:
+            image_bytes = image_file.read()
+            byte_stream = BytesIO(image_bytes)
+            attachment = await bucket_client.put_file(
+                name=file_name, content=byte_stream, mime_type=mime_type_png
+            )
+            return Attachment(
+                title=file_name, url=attachment.get("url"), type=mime_type_png
+            )
 
 
 def start() -> None:
     # TODO:
     #  1. Create DialModelClient
+    client = DialModelClient(
+        endpoint=DIAL_CHAT_COMPLETIONS_ENDPOINT,
+        deployment_name="gpt-4o",
+        api_key=API_KEY,
+    )
     #  2. Upload image (use `_put_image` method )
+    attachment = asyncio.run(_put_image())
     #  3. Print attachment to see result
+    print(attachment)
     #  4. Call chat completion via client with list containing one Message:
     #    - role: Role.USER
     #    - content: "What do you see on this picture?"
     #    - custom_content: CustomContent(attachments=[attachment])
     #  ---------------------------------------------------------------------------------------------------------------
-    #  Note: This approach uploads the image to DIAL bucket and references it via attachment. The key benefit of this
-    #        approach that we can use Models from different vendors (OpenAI, Google, Anthropic). The DIAL Core
-    #        adapts this attachment to Message content in appropriate format for Model.
-    #  TRY THIS APPROACH WITH DIFFERENT MODELS!
-    #  Optional: Try upload 2+ pictures for analysis
-    raise NotImplementedError
+    client.get_completion(
+        [
+            Message(
+                role=Role.USER,
+                content="What do you see on this picture?",
+                custom_content=CustomContent(attachments=[attachment]),
+            )
+        ]
+    )
 
 
 start()
--- task/text_to_image/task_tti.py
+++ task/text_to_image/task_tti.py
@@ -1,6 +1,7 @@
 import asyncio
 from datetime import datetime
 
+from task._models import custom_content
 from task._models.custom_content import Attachment
 from task._utils.constants import API_KEY, DIAL_URL, DIAL_CHAT_COMPLETIONS_ENDPOINT
 from task._utils.bucket_client import DialBucketClient
@@ -8,13 +9,15 @@ from task._utils.model_client import DialModelClient
 from task._models.message import Message
 from task._models.role import Role
 
+
 class Size:
     """
     The size of the generated image.
     """
-    square: str = '1024x1024'
-    height_rectangle: str = '1024x1792'
-    width_rectangle: str = '1792x1024'
+
+    square: str = "1024x1024"
+    height_rectangle: str = "1024x1792"
+    width_rectangle: str = "1792x1024"
 
 
 class Style:
@@ -23,6 +26,7 @@ class Style:
      - Vivid causes the model to lean towards generating hyper-real and dramatic images.
      - Natural causes the model to produce more natural, less hyper-real looking images.
     """
+
     natural: str = "natural"
     vivid: str = "vivid"
 
@@ -32,26 +36,64 @@ class Quality:
     The quality of the image that will be generated.
      - ‘hd’ creates images with finer details and greater consistency across the image.
     """
+
     standard: str = "standard"
     hd: str = "hd"
+
 
 async def _save_images(attachments: list[Attachment]):
     # TODO:
     #  1. Create DIAL bucket client
     #  2. Iterate through Images from attachments, download them and then save here
     #  3. Print confirmation that image has been saved locally
-    raise NotImplementedError
+    async with DialBucketClient(
+        api_key=API_KEY,
+        base_url=DIAL_URL,
+    ) as bucket_client:
+        for idx, attachment in enumerate(attachments):
+            if not attachment.url:
+                continue
+            image_bytes = await bucket_client.get_file(attachment.url)
+            file_name = (
+                f"generated_image_{idx}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+            )
+            with open(file_name, "wb") as image_file:
+                image_file.write(image_bytes)
+            print(f"Image saved locally as {file_name}")
 
 
 def start() -> None:
     # TODO:
     #  1. Create DialModelClient
+    client = DialModelClient(
+        endpoint=DIAL_CHAT_COMPLETIONS_ENDPOINT,
+        deployment_name="dall-e-3",
+        api_key=API_KEY,
+    )
     #  2. Generate image for "Sunny day on Bali"
+    user_input = (
+        "A sunny day on Bali with clear blue skies, palm trees, and a pristine beach."
+    )
+    response = client.get_completion(
+        messages=[
+            Message(
+                role=Role.USER,
+                content=user_input,
+            )
+        ],
+        custom_fields={
+            "size": Size.square,
+            "style": Style.natural,
+            "quality": Quality.hd,
+        },
+    )
     #  3. Get attachments from response and save generated message (use method `_save_images`)
+    if custom_content := response.custom_content:
+        if attachments := custom_content.attachments:
+            asyncio.run(_save_images(attachments))
     #  4. Try to configure the picture for output via `custom_fields` parameter.
     #    - Documentation: See `custom_fields`. https://dialx.ai/dial_api#operation/sendChatCompletionRequest
     #  5. Test it with the 'imagegeneration@005' (Google image generation model)
-    raise NotImplementedError
 
 
 start()
